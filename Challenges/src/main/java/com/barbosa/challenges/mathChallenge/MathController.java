b8d4fc93bba25d867a31bbf7160ec1cc2a556b7e
GiselleBarbosa <g88barbosa@gmail.com>
2024-10-24 22:34:35 -0300

    [desafio: calculadora] trata exceções - ControllerAdvice - ResponseStatus - ExceptionHandler

---
+++ Challenges/src/main/java/com/barbosa/challenges/mathChallenge/MathController.java
@@ -0,0 +1,40 @@
+package com.barbosa.challenges.mathChallenge;
+
+import com.barbosa.challenges.mathChallenge.exceptions.UnsupportedMathOperationException;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class MathController {
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(value = "/sum/{number1}/{number2}", method = RequestMethod.GET)
+    public Double sum(@PathVariable(value = "number1") String number1, @PathVariable(value = "number2") String number2)
+            throws Exception {
+
+        if (!isNumeric(number1) || !isNumeric(number2)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
+        }
+        return convertToDouble(number1) + convertToDouble(number2);
+    }
+
+    private Double convertToDouble(String strNumber) {
+        if (strNumber == null) return 0D;
+        String number = strNumber.replaceAll(",", ".");
+        if (isNumeric(number)) {
+            return Double.parseDouble(number);
+        }
+        return 0D;
+    }
+
+    private static boolean isNumeric(String strNumber) {
+        if (strNumber == null) return false;
+        String number = strNumber.replaceAll(",", ".");
+
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+}
