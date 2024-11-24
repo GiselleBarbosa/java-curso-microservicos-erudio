8465fb3a73da1a9e9eaed72933fe3163f0ef7a70
GiselleBarbosa <g88barbosa@gmail.com>
2024-11-24 12:27:45 -0300

    feat: consumindo o endpoint de Cambio Service no Book Service

--- 05-book-service/src/main/java/br/com/barbosa/controller/BookController.java
+++ 05-book-service/src/main/java/br/com/barbosa/controller/BookController.java
@@ -2,12 +2,16 @@ package br.com.barbosa.controller;
 
 import br.com.barbosa.model.Book;
 import br.com.barbosa.repository.BookRepository;
+import br.com.barbosa.response.Cambio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("book-service")
@@ -24,10 +28,26 @@ public class BookController {
             @PathVariable("id") Long id,
             @PathVariable("currency") String currency
     ) {
+
         var book = repository.findById(id).orElseThrow(() -> new RuntimeException("Book not found"));
+
+        HashMap<String, String> params = new HashMap<>();
+        params.put("amount", book.getPrice().toString());
+        params.put("from", "USD");
+        params.put("to", currency);
+
+        var response = new RestTemplate()
+                .getForEntity("http://localhost:8000/cambio-service/"
+                                + "{amount}/{from}/{to}",
+                        Cambio.class,
+                        params
+                );
+
+        var cambio = response.getBody();
 
         var port = environment.getProperty("local.server.port");
         book.setEnvironment(port);
+        book.setPrice(cambio.getConvertedValue());
 
         return book;
     }
--- 05-book-service/src/main/java/br/com/barbosa/response/Cambio.java
+++ 05-book-service/src/main/java/br/com/barbosa/response/Cambio.java
@@ -1,39 +1,28 @@
-package br.com.barbosa.model;
-
-import jakarta.persistence.*;
+package br.com.barbosa.response;
 
 import java.io.Serializable;
-import java.math.BigDecimal;
 import java.util.Objects;
 
-@Entity(name = "cambio")
 public class Cambio implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "from_currency", nullable = false, length = 3)
     private String from;
 
-    @Column(name = "to_currency", nullable = false, length = 3)
     private String to;
 
-    @Column(nullable = false)
-    private BigDecimal conversionFactor;
+    private Double conversionFactor;
 
-    @Transient
-    private BigDecimal convertedValue;
+    private Double convertedValue;
 
-    @Transient
     private String environment;
 
     public Cambio() {
     }
 
-    public Cambio(Long id, String from, String to, BigDecimal conversionFactor, BigDecimal convertedValue, String environment) {
+    public Cambio(Long id, String from, String to, Double conversionFactor, Double convertedValue, String environment) {
         this.id = id;
         this.from = from;
         this.to = to;
@@ -66,19 +55,19 @@ public class Cambio implements Serializable {
         this.to = to;
     }
 
-    public BigDecimal getConversionFactor() {
+    public Double getConversionFactor() {
         return conversionFactor;
     }
 
-    public void setConversionFactor(BigDecimal conversionFactor) {
+    public void setConversionFactor(Double conversionFactor) {
         this.conversionFactor = conversionFactor;
     }
 
-    public BigDecimal getConvertedValue() {
+    public Double getConvertedValue() {
         return convertedValue;
     }
 
-    public void setConvertedValue(BigDecimal convertedValue) {
+    public void setConvertedValue(Double convertedValue) {
         this.convertedValue = convertedValue;
     }
 
