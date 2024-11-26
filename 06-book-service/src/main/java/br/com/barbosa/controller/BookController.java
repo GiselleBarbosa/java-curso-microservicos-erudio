6d7376db6a3f17fafb8d1878b8c8514b901280f9
GiselleBarbosa <g88barbosa@gmail.com>
2024-11-26 20:05:44 -0300

    feat: adiciona dependencia do eureka cliente no Book Service [fix] url do eureka no arquivo de configuracoes

---
+++ 06-book-service/src/main/java/br/com/barbosa/controller/BookController.java
@@ -0,0 +1,42 @@
+package br.com.barbosa.controller;
+
+import br.com.barbosa.model.Book;
+import br.com.barbosa.proxy.CambioProxy;
+import br.com.barbosa.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("book-service")
+public class BookController {
+
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private BookRepository repository;
+
+    @Autowired
+    private CambioProxy proxy;
+
+    @GetMapping(value = "/{id}/{currency}")
+    public Book getBook(
+            @PathVariable("id") Long id,
+            @PathVariable("currency") String currency
+    ) {
+
+        var book = repository.findById(id).orElseThrow(() -> new RuntimeException("Book not found"));
+
+        var cambio = proxy.getCambio(book.getPrice(), "USD", currency );
+
+        var port = environment.getProperty("local.server.port");
+        book.setEnvironment(port);
+        book.setPrice(cambio.getConvertedValue());
+
+        return book;
+    }
+}
