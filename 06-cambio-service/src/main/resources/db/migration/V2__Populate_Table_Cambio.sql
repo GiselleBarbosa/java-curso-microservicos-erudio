3ad2dfabd0a1eee95eaae16c5d8439b0a164c705
GiselleBarbosa <g88barbosa@gmail.com>
2024-11-26 19:54:29 -0300

    feat: adiciona dependencia do eureka cliente no Cambio Service

---
+++ 06-cambio-service/src/main/resources/db/migration/V2__Populate_Table_Cambio.sql
@@ -0,0 +1,8 @@
+﻿INSERT INTO `cambio` (`from_currency`, `to_currency`, `conversion_factor`) VALUES
+                     ('USD', 'BRL', 5.81),
+                     ('USD', 'EUR', 0.94),
+                     ('USD', 'GBP', 0.84),
+                     ('USD', 'ARS', 350.30),
+                     ('USD', 'CLP', 877.50),
+                     ('USD', 'COP', 4014.75),
+                     ('USD', 'MXN', 18.43);
