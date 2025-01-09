bd939e4c8cba11be60d27dc557990840bbb6c267
GiselleBarbosa <g88barbosa@gmail.com>
2025-01-09 16:15:42 -0300

    feat: implementa circuit break com resilience4j e adiciona retry

---
+++ 06-book-service/src/main/java/br/com/barbosa/controller/FooBarController.java
@@ -0,0 +1,25 @@
+package br.com.barbosa.controller;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("book-service")
+public class FooBarController {
+
+    private Logger logger = org.slf4j.LoggerFactory.getLogger(FooBarController.class);
+
+    @GetMapping("/foo-bar")
+    @Retry(name = "foo-bar")
+    public String fooBar() {
+        logger.info("Request for to foo-bar is received!");
+        var response = new RestTemplate()
+                .getForEntity("http://localhost:8080/foo-bar", String.class);
+        // return "Foo Bar!!";
+        return response.getBody();
+    }
+}
