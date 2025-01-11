922e9cf7493ece5323eaab48745b087ea1792bdc
GiselleBarbosa <g88barbosa@gmail.com>
2025-01-11 10:34:29 -0300

    feat: implementa RateLimiter para limitar quantidade de requisicoes

--- 06-book-service/src/main/java/br/com/barbosa/controller/FooBarController.java
+++ 06-book-service/src/main/java/br/com/barbosa/controller/FooBarController.java
@@ -1,6 +1,7 @@
 package br.com.barbosa.controller;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,14 +16,15 @@ public class FooBarController {
     private Logger logger = org.slf4j.LoggerFactory.getLogger(FooBarController.class);
 
     @GetMapping("/foo-bar")
-//    @Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
-    @CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
+    // @Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
+    // @CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
+    @RateLimiter(name = "default")
     public String fooBar() {
         logger.info("Request for to foo-bar is received!");
-        var response = new RestTemplate()
-                .getForEntity("http://localhost:8080/foo-bar", String.class);
-        // return "Foo Bar!!";
-        return response.getBody();
+//        var response = new RestTemplate()
+//                .getForEntity("http://localhost:8080/foo-bar", String.class);
+        return "Foo Bar!!";
+//        return response.getBody();
     }
 
     public String fallbackMethod(Exception ex) {
