89778cc88cfc9f23689747404f9dbcc4c0a5f504
GiselleBarbosa <g88barbosa@gmail.com>
2025-01-11 10:38:36 -0300

    feat: implementa Bulkhead para limitar execuções simultâneas

--- 06-book-service/src/main/java/br/com/barbosa/controller/FooBarController.java
+++ 06-book-service/src/main/java/br/com/barbosa/controller/FooBarController.java
@@ -1,5 +1,6 @@
 package br.com.barbosa.controller;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
@@ -16,9 +17,10 @@ public class FooBarController {
     private Logger logger = org.slf4j.LoggerFactory.getLogger(FooBarController.class);
 
     @GetMapping("/foo-bar")
-    // @Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
-    // @CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
-    @RateLimiter(name = "default")
+//  @Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
+//  @CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
+//  @RateLimiter(name = "default")
+    @Bulkhead(name = "default")
     public String fooBar() {
         logger.info("Request for to foo-bar is received!");
 //        var response = new RestTemplate()
