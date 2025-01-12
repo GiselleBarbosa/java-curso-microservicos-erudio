3c598d60489b224f6852199e5943f1e2874c410c
GiselleBarbosa <g88barbosa@gmail.com>
2025-01-12 17:47:49 -0300

    feat: configurando o Swagger OpenAPI no Book Microservice

--- 06-book-service/src/main/java/br/com/barbosa/controller/BookController.java
+++ 06-book-service/src/main/java/br/com/barbosa/controller/BookController.java
@@ -3,6 +3,8 @@ package br.com.barbosa.controller;
 import br.com.barbosa.model.Book;
 import br.com.barbosa.proxy.CambioProxy;
 import br.com.barbosa.repository.BookRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Book Endpoint")
 @RestController
 @RequestMapping("book-service")
 public class BookController {
@@ -23,6 +26,7 @@ public class BookController {
     @Autowired
     private CambioProxy proxy;
 
+    @Operation(summary = "Find a specific book by your ID and currency")
     @GetMapping(value = "/{id}/{currency}")
     public Book getBook(
             @PathVariable("id") Long id,
--- 06-book-service/src/main/java/br/com/barbosa/controller/FooBarController.java
+++ 06-book-service/src/main/java/br/com/barbosa/controller/FooBarController.java
@@ -4,18 +4,22 @@ import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@Tag(name = "Foo Bar Endpoint")
 @RestController
 @RequestMapping("book-service")
 public class FooBarController {
 
     private Logger logger = org.slf4j.LoggerFactory.getLogger(FooBarController.class);
 
+    @Operation(summary = "Foo-bar!!")
     @GetMapping("/foo-bar")
 //  @Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
 //  @CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
